687acfe8e00994dac53cff6f9615dacccb99a747
wangxiaohui <wangxiaohui@ixhui.com>
2013-06-25 21:57:32 +0800

    入口文件

---
+++ admin.php
@@ -0,0 +1,8 @@
+<?php
+	//定义项目名称
+	define('APP_NAME', 'Admin');
+	//定义项目目录
+	define('APP_PATH', './admin/');
+	//引入框架
+	include '../ThinkPHP/ThinkPHP.php';
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	//定义项目名称
+	define('APP_NAME', 'Api');
+	//定义项目目录
+	define('APP_PATH', './api/');
+	//引入框架
+	include '../ThinkPHP/ThinkPHP.php';
+?>
